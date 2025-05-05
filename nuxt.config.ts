ee23523a78b526b435796db6c67e4cb64d2f4ccd
Andrew Ge-Hall <ge-hall@outlook.com>
2025-05-06 03:33:19 +1000

    feature:complete

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   eslint: {
     config: {
       standalone: false,
