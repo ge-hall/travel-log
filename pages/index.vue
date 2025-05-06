00188c64547649d089bbf402dfb58b760f9c0c6d
Andrew Ge-Hall <ge-hall@outlook.com>
2025-05-06 10:56:42 +1000

    add: hero plus sign in with github buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Index.vue</h1>
+  <div class="hero bg-base-200 container mx-auto my-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels with this simple travel log.
+        </p>
+        <a class="btn btn-accent">Sign In with GitHub <Icon name="tabler:brand-github" size="24" /></a>
+      </div>
+    </div>
+  </div>
 </template>
