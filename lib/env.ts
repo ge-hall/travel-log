e7805a58842c0e297337db0811da25fb5736c5dc
Andrew Ge-Hall IPA:/gər hɔl/ <ge-hall@outlook.com>
2025-05-07 15:59:55 +1000

    add: better-auth (#8)

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,8 @@ const EnvSchema = z.object({
   PORT: z.coerce.number().default(3000),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
   // DATABASE_URL: z.string(),
   // JWT_SECRET: z.string(),
   // JWT_EXPIRES_IN: z.string(),
