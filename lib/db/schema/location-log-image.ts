83971be17d11442f3ef2fe52db1f869c7c0426a3
Andrew Ge-Hall <ge-hall@outlook.com>
2025-05-07 15:56:27 +1000

    add: better-auth

--- lib/db/schema/location-log-image.ts
+++ lib/db/schema/location-log-image.ts
@@ -1,4 +1,5 @@
 import { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { user } from './auth'
 import { location } from './location'
 
 export const locationLog = sqliteTable('locationLog', {
@@ -12,6 +13,7 @@ export const locationLog = sqliteTable('locationLog', {
   locationId: int()
     .notNull()
     .references(() => location.id),
+  userId: int().notNull().references(() => user.id, { onDelete: 'cascade' }),
   createdAt: int()
     .notNull()
     .$default(() => Date.now()),
