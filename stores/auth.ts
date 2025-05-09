a097078af82ba0b905f68b07536c77295086b06e
Andrew Ge-Hall IPA:/gər hɔl/ <ge-hall@outlook.com>
2025-05-10 07:07:09 +1000

    add:ssr auth session caching (#13)

--- stores/auth.ts
+++ stores/auth.ts
@@ -3,9 +3,13 @@ import { createAuthClient } from 'better-auth/vue';
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore('authStore', () => {
-  const session = authClient.useSession();
-  const user = computed(() => session.value.data?.user);
-  const loading = computed(() => session.value.isPending || session.value.isRefetching);
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending);
 
   async function signIn() {
     await authClient.signIn.social({
@@ -19,6 +23,7 @@ export const useAuthStore = defineStore('authStore', () => {
   }
 
   return {
+    init,
     loading,
     signIn,
     signOut,
