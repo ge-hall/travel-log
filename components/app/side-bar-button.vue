42b75821fae6de5bb0cadca093dbd9f0205cba66
Andrew Ge-Hall <ge-hall@outlook.com>
2025-05-10 14:42:05 +1000

    sidebar animated with persisted client state; new side-bar-button component;

---
+++ components/app/side-bar-button.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const route = useRoute();
+const props = defineProps({
+  label: String,
+  showLabel: Boolean,
+  icon: String,
+  to: String,
+
+});
+</script>
+
+<template>
+  <div
+    :class="{ 'md:tooltip  md:tooltip-right': !showLabel }"
+    :data-tip="props.label"
+  >
+    <NuxtLink
+      :to="props.to"
+      :class="{ 'bg-base-200': route.name === props.to, 'border-y-gray-700': route.name == props.to, 'justify-center': !showLabel, 'justify-start': showLabel }"
+      class="flex flex-nowrap gap-2 p-2 hover:bg-base-300 hover:cursor-pointer border border-base-100"
+    >
+      <Icon
+        :name="props.icon"
+        size="24"
+      />
+      <Transition name="grow">
+        <span v-if="showLabel">
+          {{ props.label }}</span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.3s ease;
+}
+
+.grow-leave-active {
+  animation: grow 0.2s ease reverse;
+}
+
+@keyframes grow {
+  0% {
+    opacity: 0;
+    transform: scale(0);
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+</style>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,3 +1,65 @@
+<script setup lang="ts">
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem('isSidebarOpen') === 'true';
+});
+const toggleSidebar = () => {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem('isSidebarOpen', isSidebarOpen.value.toString());
+};
+</script>
+
 <template>
-  <h1>You are logged in!</h1>
+  <div class="flex-1 flex ">
+    <!-- sidebar drawer -->
+    <div
+      class=" bg-base-100 transition-all transition-duration-400"
+      :class="{ 'w-64': isSidebarOpen, 'w-12': !isSidebarOpen }"
+    >
+      <!-- chevron button -->
+      <div
+        class="flex hover:bg-base-300  hover:cursor-pointer p-2"
+        :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }"
+        @click="toggleSidebar"
+      >
+        <Icon
+          v-if="isSidebarOpen"
+          name="tabler:chevron-left"
+          size="32"
+        />
+        <Icon
+          v-if="!isSidebarOpen"
+          name="tabler:chevron-right"
+          size="32"
+        />
+      </div>
+
+      <!-- action button -->
+      <div class="flex flex-col gap-0 mx-0.5 ">
+        <AppSideBarButton
+          :show-label="isSidebarOpen"
+          to="dashboard"
+          label="Locations"
+          icon="tabler:map"
+        />
+
+        <AppSideBarButton
+          :show-label="isSidebarOpen"
+          to="AddLocation"
+          label="Add Location"
+          icon="tabler:plus"
+        />
+
+        <div class="divider" />
+        <AppSideBarButton
+          :show-label="isSidebarOpen"
+          to="sign-out"
+          label="Sign Out"
+          icon="tabler:logout"
+        />
+      </div>
+    </div>
+    <div class="flex-1 bg-gray-600" />
+  </div>
 </template>
